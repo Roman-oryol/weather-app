accb9352cb2934342221bace6dff33fd9e430c29
Roman Oryol <roman.oryol83@gmail.com>
2025-05-03 13:24:39 +0300

    Добавить файл weatherIcons.js с иконками погоды

---
+++ src/js/utils/weatherIcons.js
@@ -0,0 +1,47 @@
+import clearDay from '../../assets/img/weather-icons/clear-day.svg';
+import clearNight from '../../assets/img/weather-icons/clear-night.svg';
+import cloudy from '../../assets/img/weather-icons/cloudy.svg';
+import fog from '../../assets/img/weather-icons/fog.svg';
+import hail from '../../assets/img/weather-icons/hail.svg';
+import partlyCloudyDay from '../../assets/img/weather-icons/partly-cloudy-day.svg';
+import partlyCloudyNight from '../../assets/img/weather-icons/partly-cloudy-night.svg';
+import rainSnowShowersDay from '../../assets/img/weather-icons/rain-snow-showers-day.svg';
+import rainSnowShowersNight from '../../assets/img/weather-icons/rain-snow-showers-night.svg';
+import rainShow from '../../assets/img/weather-icons/rain-snow.svg';
+import rain from '../../assets/img/weather-icons/rain.svg';
+import showersDay from '../../assets/img/weather-icons/showers-day.svg';
+import showersNight from '../../assets/img/weather-icons/showers-night.svg';
+import sleet from '../../assets/img/weather-icons/sleet.svg';
+import snowShowersDay from '../../assets/img/weather-icons/snow-showers-day.svg';
+import snowShowersNight from '../../assets/img/weather-icons/snow-showers-night.svg';
+import snow from '../../assets/img/weather-icons/snow.svg';
+import thunderRain from '../../assets/img/weather-icons/thunder-rain.svg';
+import thunderShowersDay from '../../assets/img/weather-icons/thunder-showers-day.svg';
+import thunderShowersNight from '../../assets/img/weather-icons/thunder-showers-night.svg';
+import thunder from '../../assets/img/weather-icons/thunder.svg';
+import wind from '../../assets/img/weather-icons/wind.svg';
+
+export const weatherIcons = {
+  'clear-day': clearDay,
+  'clear-night': clearNight,
+  cloudy: cloudy,
+  fog: fog,
+  hail: hail,
+  'partly-cloudy-day': partlyCloudyDay,
+  'partly-cloudy-night': partlyCloudyNight,
+  'rain-snow-showers-day': rainSnowShowersDay,
+  'rain-snow-showers-night': rainSnowShowersNight,
+  'rain-snow': rainShow,
+  rain: rain,
+  'showers-day': showersDay,
+  'showers-night': showersNight,
+  sleet: sleet,
+  'snow-showers-day': snowShowersDay,
+  'snow-showers-night': snowShowersNight,
+  snow: snow,
+  'thunder-rain': thunderRain,
+  'thunder-showers-day': thunderShowersDay,
+  'thunder-showers-night': thunderShowersNight,
+  thunder: thunder,
+  wind: wind,
+};
